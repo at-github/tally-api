8d174545f5d6bfe6ca2e70897d7211f3df1789c2
Tarik Amar <tarik.amar@gmail.com>
2023-10-25 13:47:34 +0000

    Add favicon

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, url_for, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 @app.route('/')
 
