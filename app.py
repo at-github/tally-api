5d7f732e2c668fa65d3b93cab4b604269417478a
Tarik Amar <tarik.amar@gmail.com>
2023-10-26 09:00:43 +0000

    Handle unhandled verb

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def fake_entity(amount=10, date='2023-10-26'):
 
 app = Flask(__name__)
 
-@app.route('/favicon.ico')
+@app.get('/favicon.ico')
 def favicon():
     return send_from_directory(
         os.path.join(app.root_path, 'static'),
@@ -14,13 +14,21 @@ def favicon():
         mimetype='image/vnd.microsoft.icon'
     )
 
-@app.route('/transactions')
+@app.get('/transactions')
 def get_transactions():
     return [fake_entity(), fake_entity()], 200
 
 @app.errorhandler(404)
-def page_not_found(error):
+def respond_not_found(error):
+    return _respond_error('This page does not exist', error.code)
+
+@app.errorhandler(405)
+def respond_not_allowed(error):
+    return _respond_error('Your are not allowed', error.code)
+
+# Private
+def _respond_error(message, status_code=400):
     return {
-        'message': 'This page does not exist',
-        'status': error.code
-    }, error.code
+        'message': message,
+        'status': status_code
+    }, status_code
