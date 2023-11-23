346c9ad9776fa73033e53f32116e205cd823c12e
Tarik Amar <tarik.amar@gmail.com>
2023-11-23 12:57:36 +0000

    ORM - Create database.py file to configure database

---
+++ database.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    ENV: str
+    DEBUG: bool = True
+    DB_HOST: str
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
+
+DATABASE_URL = "postgresql://{user}:{password}@{host}/{db_name}".format(
+    user=settings.DB_USER,
+    password=settings.DB_PASSWORD,
+    host=settings.DB_HOST,
+    db_name=settings.DB_NAME
+)
+
+engine = create_engine(DATABASE_URL)
+
+"""
+Each instance of the SessionLocal class will be a database session.
+The class itself is not a database session yet.
+But once we create an instance of the SessionLocal class,
+this instance will be the actual database session.
+"""
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+"""
+Later we will inherit from this class
+to create each of the database models or classes (the ORM models)
+"""
+Base = declarative_base()
