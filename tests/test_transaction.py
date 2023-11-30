7474717d3c9338e5c7c5a123fd5e4681cbff1149
Tarik Amar <tarik.amar@gmail.com>
2023-11-30 10:50:46 +0000

    Test - DB for each test and create context for the first test

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from fastapi.testclient import TestClient
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import pytest
 
 from app import app, get_db
+import models.transaction as models
 
 
 class Settings(BaseSettings):
@@ -18,21 +20,15 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-
 DATABASE_URL = "postgresql://{user}:{password}@{host}/{db_name}".format(
     user=settings.DB_USER,
     password=settings.DB_PASSWORD,
     host=settings.DB_HOST,
     db_name=settings.DB_NAME
 )
-
 engine = create_engine(DATABASE_URL)
-
 TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 Base = declarative_base()
-
 Base.metadata.create_all(bind=engine)
 
 
@@ -49,11 +45,31 @@ app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 
-def test_read_transaction():
-    response = client.get('/transactions/1')
+# GET /transactions/{id}
+def test_read_existing_transaction(db_session):
+    # Context
+    amount = 700
+    date = "2023-11-29"
+    db_transaction = models.Transaction(
+        amount=amount,
+        date=date
+    )
+    db_session.add(db_transaction)
+    db_session.commit()
+    db_session.refresh(db_transaction)
+
+    # Action
+    response = client.get('/transactions/{id}'.format(id=db_transaction.id))
+
+    # Assertions
     assert response.status_code == 200
     assert response.json() == {
-        "id": 1,
-        "amount": 800,
-        "date": "2024-01-01"
+        "id": db_transaction.id,
+        "amount": amount,
+        "date": date
     }
+
+
+@pytest.fixture(autouse=True)
+def db_session():
+    yield TestSessionLocal()
